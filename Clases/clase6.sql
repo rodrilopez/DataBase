c44a66a50cf34fe687a93f8ab4ea85a7e030396b
Rodri Lopez <rodrilopezypl@gmail.com>
2017-07-31 11:10:15 -0300

    clase 6

--- Clases/clase6.sql
+++ Clases/clase6.sql
@@ -12,14 +12,14 @@ ORDER BY last_name;
 
 SELECT actor_id
 FROM actor
-WHERE actor_id NOT IN (SELECT DISTINCT actor_id
+WHERE EXISTS (SELECT DISTINCT actor_id
 				  	   FROM film_actor);
 				  	   
 -- 3
 
 SELECT c.customer_id, c.first_name, c.last_name
 FROM rental r1, customer c
-WHERE NOT EXISTS (SELECT * 
+WHERE EXISTS (SELECT * 
 				  FROM rental r2
 				  WHERE r1.customer_id = r2.customer_id
 				  AND r1.rental_id <> r2.rental_id )
