c0bec1424019c77f747689213d7034b493d868f4
Rodri Lopez <rodrilopezypl@gmail.com>
2017-08-03 05:07:15 -0300

    clase 15

--- Clases/clase15.sql
+++ Clases/clase15.sql
@@ -30,5 +30,32 @@ GROUP BY 1,2,3,4,5,6,7;
 
 
 -- ej3
-SELECT category, total_sales
 
+DROP VIEW IF EXISTS sales_by_film_category2
+
+CREATE VIEW sales_by_film_category2 AS
+SELECT name, SUM(amount) AS total_rental
+FROM category
+JOIN film_category USING (category_id)
+JOIN film USING (film_id)
+JOIN inventory USING (film_id)
+JOIN rental USING (inventory_id)
+JOIN payment USING (rental_id)
+GROUP BY name
+
+SELECT *
+FROM sales_by_film_category2
+
+
+-- ej4 
+DROP VIEW IF EXISTS actor_information
+
+CREATE VIEW actor_information AS
+SELECT actor_id, CONCAT_WS(' ', first_name, last_name) AS actor, COUNT(film.film_id) AS total_films
+FROM actor
+JOIN film_actor USING (actor_id)
+JOIN film USING (film_id)
+GROUP BY actor_id
+
+SELECT *
+FROM actor_information
